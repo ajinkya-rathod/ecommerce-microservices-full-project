3ac8ce762d9ea49879ff431830aa540f28c9fb7a
Ajinkya Rathod <ajinkayrathod10@gmail.com>
2026-08-28 00:48:00 +0530

    worked on authmapper security classes and exception handling

--- userservice-project/src/main/java/com/userservice/repo/UserRepository.java
+++ userservice-project/src/main/java/com/userservice/repo/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    boolean existByEmail(String email);
+    boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
 }
